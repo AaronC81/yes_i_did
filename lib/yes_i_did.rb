d070d29b5c5066cfb65feef7aba5a9d094de4518
Aaron Christiansen <aaronc20000@gmail.com>
2020-04-04 16:12:03 +0100

    Initial commit

---
+++ lib/yes_i_did.rb
@@ -0,0 +1,56 @@
+def yes
+  # This is a proc so that it can return from the "yes" function
+  error = proc do |msg|
+    puts IRB::Color.colorize("yes: #{msg}", ['RED'])
+    return
+  end
+
+  # Get the invalid name and the suggestion replacements
+  exception = $!
+
+  error.("last command didn't raise an exception") if exception.nil?
+  error.("unsupported error type #{exception.class}") unless exception.is_a?(NameError)
+
+  corrections = exception.corrections
+  error.("there are multiple possible corrections") if corrections.length > 1
+  error.("there are no corrections") if corrections.empty?
+
+  # Get the original, incorrect name as a string
+  checker = DidYouMean::NameErrorCheckers.new(exception)
+  case checker
+  when DidYouMean::ClassNameChecker
+    original_name = checker.class_name
+  when DidYouMean::VariableNameChecker
+    original_name = checker.name
+  else
+    error.("couldn't find a matching DidYouMean checker - this is a bug!")
+  end
+  original_name = original_name.to_s
+
+  # Get the last IRB command run
+  previous_command = IRB.CurrentContext.io.line(-2)
+
+  # Replace the original with the correction, but only replace a complete
+  # identifier
+  replacements_made = 0
+  fixed_command = previous_command.gsub(/[a-zA-Z_][a-zA-Z_0-9]*[\?!]?/) do |match|
+    if match == original_name
+      # Replace with correction
+      corrections.first.to_s
+      replacements_made += 1
+    else
+      # Keep the same
+      match
+    end
+  end
+  error.('couldn\'t make replacements, sorry!') if replacements_made.zero?
+
+  # Print the altered code
+  prompt_pad = ' ' * IRB.CurrentContext.io.prompt.length
+  output_fixed_command = fixed_command.each_line.map { |line| prompt_pad + line }.join.chomp
+  message = IRB::Color.colorize(output_fixed_command, ['GREEN'])
+  puts message
+  
+  # Run it
+  eval(fixed_command, IRB.CurrentContext.workspace.binding)
+end
---
+++ yes_i_did.gemspec
@@ -0,0 +1,14 @@
+
+Gem::Specification.new do |spec|
+  spec.name          = "yes_i_did"
+  spec.version       = '1.0.0'
+  spec.authors       = ["Aaron Christiansen"]
+  spec.email         = ["hello@aaronc.cc"]
+
+  spec.summary       = %q{Re-run code with did_you_mean suggestions in IRB}
+  spec.homepage      = "https://github.com/AaronC81/yes_i_did"
+  spec.license       = "MIT"
+
+  spec.files         = ['lib/yes-i-did.rb']
+  spec.require_paths = ["lib"]
+end
